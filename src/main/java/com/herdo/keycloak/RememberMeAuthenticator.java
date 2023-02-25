23e1f743468386306602f4dad60fbb3ce6ce1f88
Stefan Over <herdosratos@googlemail.com>
2023-02-25 16:35:33 +0100

    Added author comment

--- src/main/java/com/herdo/keycloak/RememberMeAuthenticator.java
+++ src/main/java/com/herdo/keycloak/RememberMeAuthenticator.java
@@ -7,6 +7,7 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
 
+// Author: Herdo
 public class RememberMeAuthenticator implements Authenticator {
 
     @Override
@@ -38,6 +39,6 @@ public class RememberMeAuthenticator implements Authenticator {
 
     @Override
     public void close() {
-        
+
     }
 }
