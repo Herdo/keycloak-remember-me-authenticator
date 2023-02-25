a237a4093cba6b8bd1444e30343a025430590871
Stefan Over <herdosratos@googlemail.com>
2023-02-25 13:46:20 +0100

    Base implementation

---
+++ src/main/java/com/herdo/keycloak/RememberMeAuthenticator.java
@@ -0,0 +1,16 @@
+package com.herdo.keycloak;
+
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.events.Details;
+
+public class RememberMeAuthenticator implements Authenticator {
+
+    @Override
+    public void authenticate(AuthenticationFlowContext context) {
+        context.getAuthenticationSession().setAuthNote(Details.REMEMBER_ME, "true");
+        context.getEvent().detail(Details.REMEMBER_ME, "true"); 
+        context.success();
+    }
+    
+}
---
+++ src/main/java/com/herdo/keycloak/RememberMeAuthenticatorFactory.java
@@ -0,0 +1,90 @@
+package com.herdo.keycloak;
+
+import org.keycloak.Config;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.authentication.ConfigurableAuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RememberMeAuthenticatorFactory implements AuthenticatorFactory, ConfigurableAuthenticatorFactory {
+
+    public static final String PROVIDER_ID = "remember-me-authenticator";
+    private static final RememberMeAuthenticator SINGLETON = new RememberMeAuthenticator();
+    private static AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = {
+            AuthenticationExecutionModel.Requirement.REQUIRED,
+            AuthenticationExecutionModel.Requirement.ALTERNATIVE,
+            AuthenticationExecutionModel.Requirement.DISABLED
+    };
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+	@Override
+	public String getDisplayType() {
+		return "Remember Me Authenticator";
+	}
+
+	@Override
+	public String getHelpText() {
+		return "Will force the KEYCLOAK_IDENTITY* cookies to have a lifetime beyond the users session.";
+	}
+
+    @Override
+    public Authenticator create(KeycloakSession session) {
+        return SINGLETON;
+    }
+
+    @Override
+    public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+        return REQUIREMENT_CHOICES;
+    }
+
+	@Override
+	public boolean isUserSetupAllowed() {
+		return false;
+	}
+
+	@Override
+	public boolean isConfigurable() {
+		return false;
+	}
+
+	@Override
+	public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+		return Arrays.asList(AuthenticationExecutionModel.Requirement.REQUIRED).toArray(new AuthenticationExecutionModel.Requirement[0]);
+	}
+
+	@Override
+	public List<ProviderConfigProperty> getConfigProperties() {
+		return null;
+	}
+
+	@Override
+	public String getReferenceCategory() {
+		return null;
+	}
+
+	@Override
+	public void init(Config.Scope scope) {
+
+	}
+
+	@Override
+	public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+
+	}
+
+	@Override
+	public void close() {
+
+	}
+
+}
