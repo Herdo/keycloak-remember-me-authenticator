f1077f50978d76589b8c9ad5a97472c4e6ca7cc3
Stefan Over <herdosratos@googlemail.com>
2023-02-25 15:37:13 +0100

    Added missing imports

--- src/main/java/com/herdo/keycloak/RememberMeAuthenticator.java
+++ src/main/java/com/herdo/keycloak/RememberMeAuthenticator.java
@@ -3,6 +3,9 @@ package com.herdo.keycloak;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.Authenticator;
 import org.keycloak.events.Details;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
 
 public class RememberMeAuthenticator implements Authenticator {
 
